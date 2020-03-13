dc77fd0d94924cef836bdf9431528739f02d919d
jokojr97 <joko.riyadi97@gmail.com>
2020-03-13 09:55:05 +0700

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+@extends ('layout/main')
+
+@section('title', 'About Web Programming Unpas')
+
+@section('container')
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<h1 class="mt-3">About</h1>				
+			</div>
+		</div>
+	</div>
+@endsection()
---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends ('layout/main')
+
+@section('title', 'Web Programming Unpas')
+
+@section('container')
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<h1 class="mt-3">Hello, {{$nama}}!</h1>				
+			</div>
+		</div>
+	</div>
+@endsection()
